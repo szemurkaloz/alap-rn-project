f5a416541b467e6251e1232eba49aacf7e117682
szemurkaloz <szemurkaloz@gmail.com>
2023-09-23 09:10:40 +0200

    Weben is fut háttér szín még nincs

--- src/templates/Welcome.tsx
+++ src/templates/Welcome.tsx
@@ -1,7 +1,7 @@
 import { Text, View } from 'react-native';
 
 const Welcome = () => (
-  <View className="flex-1 items-center justify-center">
+  <View className="flex-1 items-center justify-center bg-green-400">
     <Text>Open up App/ to start working on your app!</Text>
   </View>
 );
